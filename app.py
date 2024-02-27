259df90b14f6e08838d9a301e35ef1c13cc6f302
webbsalad <webbbsalad@gmail.com>
2024-02-27 22:23:48 +0300

    added support for changing languages

--- app.py
+++ app.py
@@ -1,28 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
+babel = Babel(app)
+
+app.config['BABEL_DEFAULT_LOCALE'] = 'ru'
+app.config['BABEL_TRANSLATION_DIRECTORIES'] = './translations'
 
 
+def get_locale():
+    return request.args.get('lang') or \
+           request.accept_languages.best_match(['en', 'ru'])
+
+babel.init_app(app, locale_selector=get_locale)
+
+    
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", get_locale=get_locale)
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template("about.html", get_locale=get_locale)
 
 @app.route('/prog')
 def prog():
-    return render_template("prog.html")
+    return render_template("prog.html", get_locale=get_locale)
 
 @app.route('/db')
 def db():
-    return render_template("db.html")
+    return render_template("db.html", get_locale=get_locale)
 
 @app.route('/cw')
 def cw():
-    return render_template("cw.html")
+    return render_template("cw.html", get_locale=get_locale)
 
 @app.route('/swt')
 def swt():
-    return render_template("swt.html")
+    return render_template("swt.html", get_locale=get_locale)
--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,3 @@ from app import app
 if __name__ == '__main__':
     app.run(debug=False)
 
-
-#pybabel extract -F babel.cfg -o messages.pot .
-#pybabel init -i messages.pot -d translations -l en
-# pybabel compile -d translations   
